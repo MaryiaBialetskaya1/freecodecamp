9f53373d407aff5c6142ffa6f115b5b7d9f06f7c
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 11:17:12 -0400

    feat: add solution to task ManipulatingComplexObjects.js

--- ManipulatingComplexObjects.js
+++ ManipulatingComplexObjects.js
@@ -1,3 +1,28 @@
 /**
- * Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+ * Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings..
  */
+
+ const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Chloe",
+      "title": "Lovo is Love",
+      "release_year": 2002,
+      "formats": [
+        "CD",
+        "Cassette",
+        "LP",
+        "8T"
+      ]
+    }
+  ];
