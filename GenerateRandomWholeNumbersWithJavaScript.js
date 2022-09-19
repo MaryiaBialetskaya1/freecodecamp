bd24da1465a532de9537bb74d4fc721c102fe6c9
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 13:51:59 -0400

    feat: add solution to task GenerateRandomWholeNumbersWithJavaScript.js

--- GenerateRandomWholeNumbersWithJavaScript.js
+++ GenerateRandomWholeNumbersWithJavaScript.js
@@ -1,4 +1,6 @@
 //GenerateRandomWholeNumbersWithJavaScript.js
 
 //generate and return a random whole number between 0 and 9.
-
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+  }
