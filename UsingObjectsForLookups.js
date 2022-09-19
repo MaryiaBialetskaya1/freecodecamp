8171409f4ce78db389e18d12f7ab5941522d81d9
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 10:46:53 -0400

    feat: start task UsingObjectsForLookups.js

---
+++ UsingObjectsForLookups.js
@@ -0,0 +1,27 @@
+// Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+// function phoneticLookup(val) {
+//     let result = "";
+//     switch(val) {
+//       case "alpha":
+//         result = "Adams";
+//         break;
+//       case "bravo":
+//         result = "Boston";
+//         break;
+//       case "charlie":
+//         result = "Chicago";
+//         break;
+//       case "delta":
+//         result = "Denver";
+//         break;
+//       case "echo":
+//         result = "Easy";
+//         break;
+//       case "foxtrot":
+//         result = "Frank";
+//     }
+//     return result;
+//   }
+  
+//   phoneticLookup("charlie");
+
