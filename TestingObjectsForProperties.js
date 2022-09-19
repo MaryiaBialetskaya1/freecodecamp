855a70442457f7e965971a19ec8d1d6202c97b25
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 11:02:09 -0400

    feat: start task TestingObjectsForProperties.js

---
+++ TestingObjectsForProperties.js
@@ -0,0 +1,6 @@
+// Modify the function checkObj to test if an object passed to the function (obj) contains a specific property (checkProp). If the property is found, return that property's value. If not, return "Not Found".
+
+/**
+ * function checkObj(obj, checkProp) { 
+}
+ */
