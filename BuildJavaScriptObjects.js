800161b1b8fddf90cd2d55d51a527939c44772a0
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 10:01:11 -0400

    feat: start task BuildJavaScriptObjects.js

---
+++ BuildJavaScriptObjects.js
@@ -0,0 +1,4 @@
+/*
+*Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array. */
