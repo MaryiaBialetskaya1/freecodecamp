1d1f8c67fdb83e1a1a3d08d762d0cd5a2afe60d7
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 10:07:24 -0400

    feat: start task Accessing ObjectPropertiesWithDo Notatio.js

---
+++ Accessing ObjectPropertiesWithDotNotation.js
@@ -0,0 +1,3 @@
+/**
+ * Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+ */
