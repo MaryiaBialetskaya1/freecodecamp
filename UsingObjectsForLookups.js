7978b05d124da698a56acd6fef0013e86da09e96
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 10:59:12 -0400

    feat: add solution to task UsingObjectsForLookups.js

--- UsingObjectsForLookups.js
+++ UsingObjectsForLookups.js
@@ -25,3 +25,16 @@
   
 //   phoneticLookup("charlie");
 
+function phoneticLookup(val) {
+    let result = "";
+    const lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    }
+      result = lookup[val];
+      return result;
+    }
