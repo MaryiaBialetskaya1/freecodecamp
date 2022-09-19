30b8d17e3480173d0630f81b1caa77b2598777c2
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 14:09:51 -0400

    feat: start task UseTheConditionalTernaryOperator.js

---
+++ UseTheConditionalTernaryOperator.js
@@ -0,0 +1,4 @@
+//UseTheConditionalTernaryOperator.js
+
+//Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either the string Equal or the string Not Equal.
+
