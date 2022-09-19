654e75f2178e6298cb4119a2c10c4825b77c45d7
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 14:08:19 -0400

    feat: add solution to task UseTheParseIntFunctionWithARadix.js

--- UseTheParseIntFunctionWithARadix.js
+++ UseTheParseIntFunctionWithARadix.js
@@ -1,3 +1,9 @@
 //UseTheParseIntFunctionWithARadix.js
 
 //Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  
+  convertToInteger("10011");
