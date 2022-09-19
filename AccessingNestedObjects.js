9263284ae65241f24831abd1ec0dcc23cfc92bb8
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 11:20:01 -0400

    feat: start task AccessingNestedObjects.js

---
+++ AccessingNestedObjects.js
@@ -0,0 +1,3 @@
+/**
+ * Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.
+ */
