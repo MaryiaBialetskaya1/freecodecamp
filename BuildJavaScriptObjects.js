eb3ce580b86f9a5676b5123087422c5a6bd91d59
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-19 10:05:10 -0400

    feat: add solution to task BuildJavaScriptObjects.js

--- BuildJavaScriptObjects.js
+++ BuildJavaScriptObjects.js
@@ -2,3 +2,10 @@
 *Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
 
 You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array. */
+
+const myDog = {
+    name: "Leo",
+    legs: 4,
+    tails: 1,
+    friends: ["people", "food"]
+  };
