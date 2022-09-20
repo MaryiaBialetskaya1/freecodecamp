f3386e51dada261f915e649b52eed7c45b8328b8
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-20 10:49:24 -0400

    feat: add solution to tsak es6-SetDefaultParametersForYourFunctions.js

--- es6-SetDefaultParametersForYourFunctions.js
+++ es6-SetDefaultParametersForYourFunctions.js
@@ -1,4 +1,8 @@
 //es6-SetDefaultParametersForYourFunctions.js
 /**
  * Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+ *
+ * const increment = (number, value) => number + value;
  */
+
+const increment = (number, value = 1) => number + value;
