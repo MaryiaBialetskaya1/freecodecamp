9498bc5c3e02dd709d5171f4be8cda17a2b951f6
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-20 11:20:53 -0400

    feat: add solution to task es6-UseDestructuringAssignmentToExtractValuesFromObjects.js

--- es6-UseDestructuringAssignmentToExtractValuesFromObjects.js
+++ es6-UseDestructuringAssignmentToExtractValuesFromObjects.js
@@ -12,3 +12,11 @@
 //   const tomorrow = HIGH_TEMPERATURES.tomorrow;
 
 
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  const {today, tomorrow} = HIGH_TEMPERATURES;
+
+
