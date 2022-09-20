bf4b041ca2e43cee16ace053ee640e851600b948
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-20 11:12:57 -0400

    feat: start task es6-UseTheSpreadOperatorToEvaluateArraysIn-Place.js

---
+++ es6-UseTheSpreadOperatorToEvaluateArraysIn-Place.js
@@ -0,0 +1,12 @@
+//es6-UseTheSpreadOperatorToEvaluateArraysIn-Place.js
+
+//Copy all contents of arr1 into another array arr2 using the spread operator.
+
+/**
+ * const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [];
+
+console.log(arr2);
+ */
