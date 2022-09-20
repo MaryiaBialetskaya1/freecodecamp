4118f5cc72df2798ce30e00439882124455ed0dc
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-20 15:39:07 -0400

    feat: start task es6-UseDestructuringAssignmentToAssignVariablesFromArrays.js

---
+++ es6-UseDestructuringAssignmentToAssignVariablesFromArrays.js
@@ -0,0 +1,4 @@
+//es6-UseDestructuringAssignmentToAssignVariablesFromArrays.js
+
+//Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+
