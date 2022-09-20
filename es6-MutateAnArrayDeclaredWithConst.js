36fb3c38310b297f5d8f97984506e51bb397ca4f
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-20 10:23:34 -0400

    feat: start task es6-MutateAnArrayDeclaredWithConst.js

---
+++ es6-MutateAnArrayDeclaredWithConst.js
@@ -0,0 +1,11 @@
+//es6-MutateAnArrayDeclaredWithConst.js
+
+//An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
+
+//stater code
+/**
+ * const s = [5, 7, 2];
+function editInPlace() {
+}
+editInPlace();
+ */
