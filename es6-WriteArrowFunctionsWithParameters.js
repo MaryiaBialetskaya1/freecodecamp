8c2ecd3ff3aeecfd97162301e89f87d8152193c0
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-20 10:38:10 -0400

    feat: start task es6-WriteArrowFunctionsWithParameters.js

---
+++ es6-WriteArrowFunctionsWithParameters.js
@@ -0,0 +1,13 @@
+//es6-WriteArrowFunctionsWithParameters.js
+
+/**
+ * Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+ */
+
+/**
+ * var myConcat = function(arr1, arr2) {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+ */
