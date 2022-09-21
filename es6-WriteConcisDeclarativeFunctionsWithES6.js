cff73cd0ca3217a8bab336e316aa5c195ed32429
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-21 10:20:16 -0400

    feat: add solution to task es6-WriteConcisDeclarativeFunctionsWithES6.js

--- es6-WriteConcisDeclarativeFunctionsWithES6.js
+++ es6-WriteConcisDeclarativeFunctionsWithES6.js
@@ -12,3 +12,12 @@
 //   bicycle.setGear(3);
 //   console.log(bicycle.gear);
 
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
