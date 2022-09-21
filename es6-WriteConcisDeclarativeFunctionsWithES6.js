52240452437e9348ee89f24b9fb4e5257839cc7a
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-21 10:18:25 -0400

    feat: start task es6-WriteConcisDeclarativeFunctionsWithES6.js

---
+++ es6-WriteConcisDeclarativeFunctionsWithES6.js
@@ -0,0 +1,14 @@
+//es6-WriteConcisDeclarativeFunctionsWithES6.js
+
+// Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
+
+// const bicycle = {
+//     gear: 2,
+//     setGear: function(newGear) {
+//       this.gear = newGear;
+//     }
+//   };
+
+//   bicycle.setGear(3);
+//   console.log(bicycle.gear);
+
