e7819bd73f96e4b22efe71eff12be77270f389cd
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-21 10:43:43 -0400

    feat: add solution to task es6-UseGettersAndSettersToControlAccessToAnObject.js

--- es6-UseGettersAndSettersToControlAccessToAnObject.js
+++ es6-UseGettersAndSettersToControlAccessToAnObject.js
@@ -11,3 +11,21 @@
 // This is the power of a getter and a setter. You are creating an API for another user, who can get the correct result regardless of which one you track.
 
 // In other words, you are abstracting implementation details from the user.
+
+
+class Thermostat{
+    constructor(fahrenheit){
+      this._fahrenheit = fahrenheit;
+    }
+    get temperature(){
+      return 5/9 * (this._fahrenheit - 32);
+    }
+    set temperature(celsius){
+      this._fahrenheit = (celsius * 9.0/ 5 + 32);
+    }
+  }
+
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
