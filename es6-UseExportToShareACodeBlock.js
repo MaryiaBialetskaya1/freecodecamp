2f0108a321983c9e6d4f426025aae21cbe466a0c
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-21 10:51:54 -0400

    feat: add solution to task es6-UseExportToShareACodeBlock.js

--- es6-UseExportToShareACodeBlock.js
+++ es6-UseExportToShareACodeBlock.js
@@ -1,3 +1,12 @@
 //es6-UseExportToShareACodeBlock.js
 
 // There are two string-related functions in the editor. Export both of them using the method of your choice.
+
+
+export const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+
+  export const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
