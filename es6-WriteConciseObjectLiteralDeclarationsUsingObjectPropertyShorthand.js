a2242622899c948680f542b405206e78b8c80d2b
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-21 10:10:39 -0400

    feat: start task es6-WriteConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js

---
+++ es6-WriteConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
@@ -0,0 +1,3 @@
+//es6-WriteConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
+
+//Use object property shorthand with object literals to create and return an object with name, age and gender properties.
