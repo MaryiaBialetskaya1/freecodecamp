e665c13cb920c0bc46ad59fce869f866be7ee464
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-21 10:54:01 -0400

    feat: start task es6-ReuseJavaScriptCodeUsingImport.js

---
+++ es6-ReuseJavaScriptCodeUsingImport.js
@@ -0,0 +1,3 @@
+//es6-ReuseJavaScriptCodeUsingImport.js
+
+// Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
